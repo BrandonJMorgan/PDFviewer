558543fcfd0e81dfdb70127285783b0127f6b5be
Brandon <31463964+BrandonJMorgan@users.noreply.github.com>
2018-06-06 20:05:59 -0500

    PDF viewer
    This uses the Openfiledialog and built in web browser to view PDFs

---
+++ PDFView3r/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace PDFView3r
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pdfBrowser = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(298, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select PDF";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pdfBrowser
+            // 
+            this.pdfBrowser.Location = new System.Drawing.Point(12, 54);
+            this.pdfBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.pdfBrowser.Name = "pdfBrowser";
+            this.pdfBrowser.ScriptErrorsSuppressed = true;
+            this.pdfBrowser.Size = new System.Drawing.Size(756, 472);
+            this.pdfBrowser.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(780, 538);
+            this.Controls.Add(this.pdfBrowser);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.WebBrowser pdfBrowser;
+    }
+}
+
---
+++ PDFView3r/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO; //allows for file access
+
+namespace PDFView3r
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pdfBrowser.ScriptErrorsSuppressed = true; //stops annoying JS script errors. remove code to find out 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog(); // create new object
+            openFileDialog1.ShowDialog(); // shows the file dialog box
+            string pdfpath = openFileDialog1.FileName; //converts file to string for pdf viewer to access 
+            pdfBrowser.Navigate(pdfpath); // navigate to open file
+        }
+    }
+}
